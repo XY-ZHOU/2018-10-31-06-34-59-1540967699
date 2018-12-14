55e4bde5787db99f943e1718d468250099fbf004
XY-ZHOU <731986251@qq.com>
2018-12-14 16:10:39 +0800

    get the same element in two arrays

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  let arr = [];
+  for (let i = 0; i < collection_b.length; i++) {
+    if (judgeStrInArr(collection_a, collection_b[i])) {
+      arr.push(collection_b[i])
+    }
+  }
+  return arr;
+}
+
+function judgeStrInArr(arr, str) {
+  return (arr.indexOf(str) != -1);
 }
 
 module.exports = get_intersection;
