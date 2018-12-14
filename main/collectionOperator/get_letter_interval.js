70d8724b76d2d0e85505747238128bc912cde0f3
XY-ZHOU <731986251@qq.com>
2018-12-14 17:15:52 +0800

    letters are arranged in intervals

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let arr = [];
+  if (compare(number_a, number_b)) {
+    for (let i = number_a; i >= number_b; i--) {
+      arr.push(String.fromCharCode(i+96));
+    }
+  } else {
+    for (let i = number_a; i <= number_b; i++) {
+      arr.push(String.fromCharCode(i+96));
+    }
+  }
+  return arr;
+}
+
+function compare(num_a, num_b) {
+  return (num_a - num_b >= 0);
 }
 
 module.exports = get_letter_interval;
