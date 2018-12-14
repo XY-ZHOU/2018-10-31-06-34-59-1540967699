c8fb5bf44b67d43400ef1e895bd1c61fb981f074
XY-ZHOU <731986251@qq.com>
2018-12-14 15:00:11 +0800

    add get integer interval js file

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+  let arr = [];
+  if (output(number_a, number_b)) {
+    for (let i = number_a; i >= number_b; i--) {
+      arr.push(i);
+    }
+  } else {
+    for (let i = number_a; i <= number_b; i++) {
+      arr.push(i);
+    }
+  }
+  return arr;
 }
 
+function output(num_a, num_b) {
+  return (num_a - num_b >= 0);
+}
 module.exports = get_integer_interval;
-
