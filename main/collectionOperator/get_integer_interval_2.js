59e7f8ee4d662630779d5cc71f1ea827bbdcf8c0
XY-ZHOU <731986251@qq.com>
2018-12-14 20:03:26 +0800

    get integer interval of even

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,29 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let arr = [];
+  if (compare(number_a, number_b)) {
+    for (let i = number_a; i >= number_b; i--) {
+      if(judgeEven(i)){
+        arr.push(i);
+      }
+    }
+  } else {
+    for (let i = number_a; i <= number_b; i++) {
+      if(judgeEven(i)){
+        arr.push(i);
+      }
+    }
+  }
+  return arr;
+}
+
+function compare(num_a, num_b) {
+  return (num_a - num_b >= 0);
+}
+
+function judgeEven(num){
+  return num%2 == 0;
 }
 
 module.exports = get_integer_interval_2;
