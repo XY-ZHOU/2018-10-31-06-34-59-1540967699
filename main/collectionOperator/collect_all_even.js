baf932741668c7cfdc4286394ce10b7a134bfcb6
XY-ZHOU <731986251@qq.com>
2018-12-14 10:44:37 +0800

    find all even in collection

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  let arr = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeEven(parseInt(collection[i]))) {
+      arr.push(collection[i]);
+      }
+  }
+  return arr;
+}
+
+function judgeEven(num) {
+  return (num % 2 === 0);
 }
 
 module.exports = collect_all_even;
