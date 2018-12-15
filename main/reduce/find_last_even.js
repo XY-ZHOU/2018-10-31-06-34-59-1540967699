070a74dab6d803146fe6ab743518bac484773c67
XY-ZHOU <731986251@qq.com>
2018-12-15 21:48:54 +0800

    find last even

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  for (let i = collection.length - 1; i > 0; i--) {
+    if (judgeEven(collection[i])) {
+      return collection[i];
+    }
+  }
+}
+
+function judgeEven(num) {
+  return num % 2 == 0;
 }
 
 module.exports = find_last_even;
