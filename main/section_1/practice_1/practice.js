ebc8fc89c9704d9d703e3deb529d68e4734e3567
XY-ZHOU <731986251@qq.com>
2018-12-15 21:52:16 +0800

    collect same elements in collection

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  let res=[];
+  for(let i = 0; i<collection_a.length;i++){
+    if(judgeStrInArr(collection_b,collection_a[i])){
+      res.push(collection_a[i]);
+    }
+  }
+  return res;
+}
+
+function judgeStrInArr(arr, str) {
+  return (arr.indexOf(str) != -1);
 }
 
 module.exports = collect_same_elements;
