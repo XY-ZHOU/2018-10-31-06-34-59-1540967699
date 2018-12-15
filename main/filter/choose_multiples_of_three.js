c612a75fc04714c2ea43798395f2c6006262bb3f
XY-ZHOU <731986251@qq.com>
2018-12-15 13:55:58 +0800

    choose multiples of three

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
+  let arr = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeEven(parseInt(collection[i]))) {
+      arr.push(collection[i]);
+      }
+  }
+  return arr;
+}
 
-  //在这里写入代码
+function judgeEven(num) {
+  return (num % 3 === 0);
 }
 
 module.exports = choose_multiples_of_three;
