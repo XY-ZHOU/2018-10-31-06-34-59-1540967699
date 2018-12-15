0fddb38ac792b586a982120ed235e7d4cd4c2108
XY-ZHOU <731986251@qq.com>
2018-12-15 17:47:23 +0800

    double dimensional array to one with unique elements

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,29 @@
 'use strict';
 
 function double_to_one(collection) {
+  return [...new Set(double_to_one_of_all(collection))];
+}
 
-  //在这里写入代码
+function double_to_one_of_all(collection) {
+  let res=[];
+  for (let i = 0; i < collection.length; i++) {
+    if(isArrayOrNot(collection[i])){
+      outputElements(collection[i],res);
+    }else{
+      res.push(collection[i]);
+    }
+  }
+  return res;
+}
+
+function outputElements(arr,res){
+  for (let i = 0; i < arr.length; i++) {
+    res.push(arr[i]);
+  }
+}
+
+function isArrayOrNot(arr){
+  return (Object.prototype.toString.call(arr) == "[object Array]");
 }
 
 module.exports = double_to_one;
