07ebcc974c791c0c8cb7fba7582ce3b6e0ea10c7
XY-ZHOU <731986251@qq.com>
2018-12-15 13:49:32 +0800

    choose even from array

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_even(collection) {
+  let arr = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeEven(parseInt(collection[i]))) {
+      arr.push(collection[i]);
+      }
+  }
+  return arr;
+}
 
-  //在这里写入代码
+function judgeEven(num) {
+  return (num % 2 === 0);
 }
 
 module.exports = choose_even;
