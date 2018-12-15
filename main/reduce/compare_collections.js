5c28aff4dd13ee1ee57a04fbda8c3a1b99c388ef
XY-ZHOU <731986251@qq.com>
2018-12-15 20:22:36 +0800

    judge two array is the same or not

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,9 +1,19 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  if (isSameLength(collection_a, collection_b)) {
+    return false;
+  } else {
+    for (let i = 0; i < collection_a.length; i++) {
+      if (collection_a[i] != collection_b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
+function isSameLength(arr_a, arr_b) {
+  return arr_a.length != arr_b.length;
+}
 module.exports = compare_collections;
-
-
