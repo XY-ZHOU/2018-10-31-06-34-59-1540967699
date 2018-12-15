71ac497bbd913f123de2bcbc34637259c9c5ca43
XY-ZHOU <731986251@qq.com>
2018-12-15 14:04:08 +0800

    choose no repeat number

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
+  let res = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeStrInArr(res, collection[i])) {
+      res.push(collection[i]);
+    }
+  }
+  return res;
+}
 
-  //在这里写入代码
+function judgeStrInArr(array, str) {
+  return (array.indexOf(str) === -1);
 }
 
 module.exports = choose_no_repeat_number;
