a079d25caa5ffac6ffcd96cce2c20e3c24f48bf4
XY-ZHOU <731986251@qq.com>
2018-12-15 17:36:55 +0800

    double dimensional array to one

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,25 @@
 'use strict';
 
 function double_to_one(collection) {
+  let res=[];
+  for (let i = 0; i < collection.length; i++) {
+    if(isArrayOrNot(collection[i])){
+      outputElements(collection[i],res);
+    }else{
+      res.push(collection[i]);
+    }
+  }
+  return res;
+}
 
-  //在这里写入代码
+function outputElements(arr,res){
+  for (let i = 0; i < arr.length; i++) {
+    res.push(arr[i]);
+  }
+}
+
+function isArrayOrNot(arr){
+  return (Object.prototype.toString.call(arr) == "[object Array]");
 }
 
 module.exports = double_to_one;
