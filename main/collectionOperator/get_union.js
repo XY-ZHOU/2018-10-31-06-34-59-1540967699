a246049de6426e40ae82077f8f6eee14e41f1210
XY-ZHOU <731986251@qq.com>
2018-12-15 13:42:28 +0800

    remove duplicate elements from two arrays

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  let res=[];
+  outputElement(collection_a,res);
+  outputElement(collection_b,res);
+  return res;
+}
+
+function outputElement(arr,res){
+  for(let i = 0; i<arr.length; i++) {
+    if(judgeStrInArr(res, arr[i])){
+      res.push(arr[i]);
+    }
+  }
+}
+
+function judgeStrInArr(array, str) {
+  return (array.indexOf(str) == -1);
 }
 
 module.exports = get_union;
