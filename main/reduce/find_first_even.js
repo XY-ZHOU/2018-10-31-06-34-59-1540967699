dff9a619f91fa80efe4ffad51d8a12da1e4269ba
XY-ZHOU <731986251@qq.com>
2018-12-15 21:48:28 +0800

    find first even

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function find_first_even(collection) {
-  //在这里写入代码
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeEven(collection[i])) {
+      return collection[i];
+    }
+  }
+}
+
+function judgeEven(num) {
+  return num % 2 == 0;
 }
 
 module.exports = find_first_even;
-
