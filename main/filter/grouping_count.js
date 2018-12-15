19ac35eb9d308d2e712053d705fbe56e935f1eec
XY-ZHOU <731986251@qq.com>
2018-12-15 14:36:05 +0800

    group element's count in array

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function grouping_count(collection) {
+  let obj={};
+  for(let i = 0; i<collection.length; i++) {
+    if(judgeKeyInObj(obj, collection[i])){
+      obj[collection[i]] = 1 ;
+    }else{
+      obj[collection[i]]++;
+    }
+  }
+  return obj;
+}
 
-  //在这里写入代码
+function judgeKeyInObj(obj, key) {
+  return (!obj.hasOwnProperty(key));
 }
 
 module.exports = grouping_count;
