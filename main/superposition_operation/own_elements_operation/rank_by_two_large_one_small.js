d2c905f4c9bfee40434d9f0b58c86ed10ce33956
XY-ZHOU <731986251@qq.com>
2018-12-16 20:03:44 +0800

    rank by two large one small

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,19 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+
+function rank_by_two_large_one_small(collection) {
+  collection.sort(sortAsc);
+  let i = 0;
+  while (collection.length - i > 2) {
+    let temp = collection[i];
+    collection[i] = collection[i + 1];
+    collection[i + 1] = collection[i + 2];
+    collection[i + 2] = temp;
+    i += 3;
+  }
+  return collection;
+}
+
+function sortAsc(a, b) {
+  return a - b;
 }
 module.exports = rank_by_two_large_one_small;
