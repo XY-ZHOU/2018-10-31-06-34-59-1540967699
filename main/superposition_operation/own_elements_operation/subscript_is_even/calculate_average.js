70798429c1df9c37b80a83c7e09292a27ce51e7a
XY-ZHOU <731986251@qq.com>
2018-12-16 19:13:44 +0800

    calculate average

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,11 @@
 'use strict';
-var calculate_average = function(collection){
-
+var calculate_average = function(collection) {
+  let sum = 0;
+  let count = 0;
+  for (let i = 1; i < collection.length; i += 2) {
+    sum += collection[i];
+    count++;
+  }
+  return sum / count;
 };
 module.exports = calculate_average;
