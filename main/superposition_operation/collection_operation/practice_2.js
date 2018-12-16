b0aef00746d858e5f9ed93789f7cdb0982212a60
XY-ZHOU <731986251@qq.com>
2018-12-16 19:07:51 +0800

    odd number multipled three add two

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  let res = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeUneven(collection[i])) {
+      collection[i] = 3 * collection[i] + 2;
+      res.push(collection[i]);
+    }
+  }
+  return res;
 }
 
+function judgeUneven(num) {
+  return num % 2 != 0;
+}
 module.exports = hybrid_operation_to_uneven;
-
