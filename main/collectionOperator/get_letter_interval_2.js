10bde207381e0a6a3da541c0675122b579e3970f
XY-ZHOU <731986251@qq.com>
2018-12-16 21:25:55 +0800

    get letter interval common

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,8 +1,28 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  if (number_a <= number_b) {
+    return outputString(number_a, number_b);
+  } else {
+    return outputString(number_b, number_a).reverse();
+  }
 }
 
-module.exports = get_letter_interval_2;
+function outputString(num_a, num_b) {
+  let arr = [];
+  for (let i = num_a; i <= num_b; i++) {
+    if (i <= 26) {
+      arr.push(String.fromCharCode(i + 96));
+    } else if (26 < i && i <= 52) {
+      arr.push(numberToString(i, 1));
+    } else if (52 < i && i <= 78) {
+      arr.push(numberToString(i, 2));
+    }
+  }
+  return arr;
+}
 
+function numberToString(number, n) {
+  return String.fromCharCode(n + 96) + String.fromCharCode(number - 26 * n + 96);
+}
+module.exports = get_letter_interval_2;
