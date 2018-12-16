39153b052e7f69e2e959dc0c917b77e640a8d9b5
XY-ZHOU <731986251@qq.com>
2018-12-16 15:34:24 +0800

    count same elements in object common

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,37 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let res = [];
+  let groupingCountObj = grouping_count(collection);
+  for (key in groupingCountObj) {
+    let obj = {};
+    obj.key = key;
+    obj.count = groupingCountObj[key];
+    res.push(obj);
+  }
+  return res;
 }
 
+function grouping_count(collection) {
+  let groupingCountObj = {};
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeStr(collection[i])) {
+      groupingCountObj[collection[i].slice(0, 1)] = Number(collection[i].slice(2));
+    } else {
+      if (judgeKeyInObj(groupingCountObj, collection[i])) {
+        groupingCountObj[collection[i]] = 1;
+      } else {
+        groupingCountObj[collection[i]]++;
+      }
+    }
+  }
+  return groupingCountObj;
+}
+
+function judgeKeyInObj(obj, key) {
+  return (!obj.hasOwnProperty(key));
+}
+
+function judgeStr(str) {
+  var reg = /^[a-z]-\d{1,2}$/;
+  return reg.test(str);
+}
 module.exports = count_same_elements;
