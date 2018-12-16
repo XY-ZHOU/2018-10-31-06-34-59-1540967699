7861cf696a7da8eab6dfb23eb3dba0a0ec4afd82
XY-ZHOU <731986251@qq.com>
2018-12-16 19:57:39 +0800

    one add next multiply three

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,10 @@
 'use strict';
-function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+
+function one_add_next_multiply_three(collection) {
+  let arr = new Array(collection.length - 1);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = (collection[i] + collection[i + 1]) * 3;
+  }
+  return arr;
 }
 module.exports = one_add_next_multiply_three;
