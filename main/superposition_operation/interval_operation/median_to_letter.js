90d7de603b6a7e94c04ad9dd5329a86af17acd31
XY-ZHOU <731986251@qq.com>
2018-12-16 19:11:53 +0800

    median to letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function median_to_letter(collection) {
-
-  //在这里写入代码
+  let num = collection[0] + collection.length / 2;
+  if (num <= 26) {
+    return String.fromCharCode(num + 96);
+  } else {
+    return "a" + String.fromCharCode(num + 70);
+  }
 }
-
 module.exports = median_to_letter;
