b4eeadd392252e4984e04a09687dbea25b7ae0e5
XY-ZHOU <731986251@qq.com>
2018-12-16 19:08:33 +0800

    odd number multipled three add five

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,18 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeUneven(collection[i])) {
+      sum += collection[i];
+      count++;
+    }
+  }
+  return 3 * sum + 5 * count;
 }
 
+function judgeUneven(num) {
+  return num % 2 != 0;
+}
 module.exports = hybrid_operation_to_uneven;
-
