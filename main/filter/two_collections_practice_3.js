67db173a5a81c15d39391b3f0f15377efeb057aa
XY-ZHOU <731986251@qq.com>
2018-12-16 19:54:12 +0800

    choose divisible integer

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  let res = [];
+  for (let i = 0; i < collection_a.length; i++) {
+    for (let j = 0; j < collection_b.length; j++) {
+      if (judgeDivisible(collection_a[i], collection_b[j])) {
+        res.push(collection_a[i]);
+        break;
+      }
+    }
+  }
+  return res;
 }
 
+function judgeDivisible(num_a, num_b) {
+  return num_a % num_b == 0;
+}
 module.exports = choose_divisible_integer;
