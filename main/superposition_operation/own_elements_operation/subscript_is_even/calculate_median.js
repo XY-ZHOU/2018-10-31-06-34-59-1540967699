cf73d430046251fbc0bfe8a15a13be51bb5b33b5
XY-ZHOU <731986251@qq.com>
2018-12-16 19:23:19 +0800

    calculate median

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,17 @@
 'use strict';
-var calculate_median = function(collection){
-
+var calculate_median = function(collection) {
+  let count = 0;
+  for (let i = 1; i < collection.length; i += 2) {
+    count++;
+  }
+  if (judgeUneven(count)) {
+    return collection[count];
+  } else {
+    return (collection[(count / 2 - 1) * 2 + 1] + collection[count + 1]) / 2;
+  }
 };
+
+function judgeUneven(num) {
+  return num % 2 != 0;
+}
 module.exports = calculate_median;
