45b865b45b5d389c80a2bf7aa87510642b6c2e9e
XY-ZHOU <731986251@qq.com>
2018-12-16 15:56:17 +0800

    update collection by count same elements

--- main/section_3/practice_3/practice.js
+++ main/section_3/practice_3/practice.js
@@ -1,5 +1,42 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let countSameElemArr = count_same_elements(collection_a);
+  for (let i = 0; i < countSameElemArr.length; i++) {
+    if (judgeElementsExist(object_b.value, countSameElemArr[i].key)) {
+      countSameElemArr[i].count = countSameElemArr[i].count - parseInt(countSameElemArr[i].count / 3);
+    }
+  }
+  return countSameElemArr;
 }
 
+function judgeElementsExist(str_a, str_b) {
+  return str_a.indexOf(str_b) != -1;
+}
+
+function count_same_elements(collection) {
+  let res = [];
+  let groupingCountObj = grouping_count(collection);
+  for (key in groupingCountObj) {
+    let obj = {};
+    obj.key = key;
+    obj.count = groupingCountObj[key];
+    res.push(obj);
+  }
+  return res;
+}
+
+function grouping_count(collection) {
+  let groupingCountObj = {};
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeKeyInObj(groupingCountObj, collection[i])) {
+      groupingCountObj[collection[i]] = 1;
+    } else {
+      groupingCountObj[collection[i]]++;
+    }
+  }
+  return groupingCountObj;
+}
+
+function judgeKeyInObj(obj, key) {
+  return (!obj.hasOwnProperty(key));
+}
 module.exports = create_updated_collection;
