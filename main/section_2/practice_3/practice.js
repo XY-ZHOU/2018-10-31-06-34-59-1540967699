fd29a75ac8585cd91eec4a3b1d690fbb427efa85
XY-ZHOU <731986251@qq.com>
2018-12-16 15:35:09 +0800

    count same elements in collection hard

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,46 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let res = [];
+  let groupingCountObj = grouping_count(collection);
+  for (key in groupingCountObj) {
+    let obj = {};
+    obj.name = key;
+    obj.summary = groupingCountObj[key];
+    res.push(obj);
+  }
+  return res;
 }
 
+function grouping_count(collection) {
+  let groupingCountObj = {};
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeStr(collection[i])) {
+      let str = collection[i].slice(0, 1);
+      if (!judgeSummary(groupingCountObj, str)) {
+        groupingCountObj[str] = parseInt(collection[i].slice(2));
+      } else {
+        groupingCountObj[str] += parseInt(collection[i].slice(2));;
+      }
+    } else {
+      if (judgeKeyInObj(groupingCountObj, collection[i])) {
+        groupingCountObj[collection[i]] = 1;
+      } else {
+        groupingCountObj[collection[i]]++;
+      }
+    }
+  }
+  return groupingCountObj;
+}
+
+function judgeKeyInObj(obj, key) {
+  return (!obj.hasOwnProperty(key));
+}
+
+function judgeStr(str) {
+  var reg = /^([a-z](-|:)\d{1,2})|([a-z]\[\d{1,2}\])$/;
+  return reg.test(str);
+}
+
+function judgeSummary(obj, key) {
+  return obj[key];
+}
 module.exports = count_same_elements;
