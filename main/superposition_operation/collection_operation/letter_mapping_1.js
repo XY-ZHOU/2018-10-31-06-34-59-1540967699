3ca86cf71da16f04d4d96b2b2d5be246d6045d28
XY-ZHOU <731986251@qq.com>
2018-12-16 19:00:28 +0800

    even to letter

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function even_to_letter(collection) {
+  let res = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeEven(collection[i])) {
+      res.push(String.fromCharCode(collection[i] + 96));
+    }
+  }
+  return res;
+}
 
-  //在这里写入代码
+function judgeEven(num) {
+  return (num % 2 === 0);
 }
 
 module.exports = even_to_letter;
