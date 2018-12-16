75dd11d28134678ce42436c381addcd3c64c329f
XY-ZHOU <731986251@qq.com>
2018-12-16 19:40:50 +0800

    single element in collection

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,16 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function(collection) {
+  let res = [];
+  let arr = new Array(collection.length / 2);
+  for (let i = 1; i < collection.length; i += 2) {
+    arr[parseInt(i / 2)] = collection[i];
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (arr.lastIndexOf(num) === arr.indexOf(num)) {
+      res.push(num);
+    }
+  }
+  return res;
 };
 module.exports = single_element;
