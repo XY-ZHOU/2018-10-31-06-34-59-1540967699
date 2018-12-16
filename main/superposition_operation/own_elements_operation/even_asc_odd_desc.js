a4dc9dcb5a413613cfb932927a62c043e09821c2
XY-ZHOU <731986251@qq.com>
2018-12-16 19:55:28 +0800

    even asc odd desc

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,26 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
+var even_asc_odd_desc = function(collection) {
+  let evenArr = [];
+  let unevenArr = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (judgeEven(collection[i])) {
+      evenArr.push(collection[i]);
+    } else {
+      unevenArr.push(collection[i]);
+    }
+  }
+  return evenArr.sort(sortAsc).concat(unevenArr.sort(sortDesc));
+}
 
-};
+function judgeEven(num) {
+  return (num % 2 === 0);
+}
+
+function sortAsc(a, b) {
+  return a - b;
+}
+
+function sortDesc(a, b) {
+  return b - a;
+}
 module.exports = even_asc_odd_desc;
