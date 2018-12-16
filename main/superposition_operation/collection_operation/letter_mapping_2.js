c5f38b6de4f91ecbf3acf0d1519f6b20e6626de3
XY-ZHOU <731986251@qq.com>
2018-12-16 19:02:19 +0800

    average element to letter

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,9 +1,20 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+  let sum = 0;
+  let average = 0;
+  for (let i = 0; i < collection.length; i++) {
+    sum += collection[i];
+  }
+  if (judgeRemainder(sum, collection.length)) {
+    average = sum / collection.length;
+  } else {
+    average = sum / collection.length + 1;
+  }
+  return String.fromCharCode(average + 96);
 }
 
+function judgeRemainder(num_a, num_b) {
+  return num_a % num_b == 0;
+}
 module.exports = average_to_letter;
-
