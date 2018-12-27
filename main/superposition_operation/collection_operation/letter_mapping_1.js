a0e94fbd28eb8a3783005cff4c9e75640513bcea
XY-ZHOU <731986251@qq.com>
2018-12-27 18:34:12 +0800

    modify letter mapping one

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,17 +1,16 @@
 'use strict';
 
 function even_to_letter(collection) {
-  let res = [];
-  for (let i = 0; i < collection.length; i++) {
-    if (judgeEven(collection[i])) {
-      res.push(String.fromCharCode(collection[i] + 96));
-    }
+  let res = collect_all_odd(collection);
+  for (let element of collection) {
+    element = String.fromCharCode(element);
   }
   return res;
 }
 
-function judgeEven(num) {
-  return (num % 2 === 0);
+function collect_all_odd(collection) {
+  return collection.filter(function(element) {
+    return element % 2 != 0;
+  });
 }
-
 module.exports = even_to_letter;
