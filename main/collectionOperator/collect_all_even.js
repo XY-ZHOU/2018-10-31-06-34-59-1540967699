e6b80a8472d32d41d3fdc28aaf1ab3acc5c7216c
XY-ZHOU <731986251@qq.com>
2018-12-27 15:25:10 +0800

    modify collect all even

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -1,17 +1,8 @@
 'use strict';
 
 function collect_all_even(collection) {
-  let arr = [];
-  for (let i = 0; i < collection.length; i++) {
-    if (judgeEven(parseInt(collection[i]))) {
-      arr.push(collection[i]);
-      }
-  }
-  return arr;
+  return collection.filter(function(element) {
+    return element % 2 === 0;
+  });
 }
-
-function judgeEven(num) {
-  return (num % 2 === 0);
-}
-
 module.exports = collect_all_even;
