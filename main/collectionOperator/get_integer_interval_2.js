0cf46a022e44211b605d8a509f73e7c8836cd607
XY-ZHOU <731986251@qq.com>
2018-12-27 15:27:06 +0800

    modify get integer interval two

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,29 +1,15 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
+  let start = (number_a >= number_b) ? number_b : number_a;
+  let end = (number_a >= number_b) ? number_a : number_b;
   let arr = [];
-  if (compare(number_a, number_b)) {
-    for (let i = number_a; i >= number_b; i--) {
-      if(judgeEven(i)){
-        arr.push(i);
-      }
-    }
-  } else {
-    for (let i = number_a; i <= number_b; i++) {
-      if(judgeEven(i)){
-        arr.push(i);
-      }
-    }
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
   }
-  return arr;
+  let result = arr.filter(function(element) {
+    return element % 2 === 0;
+  });
+  return (number_a <= number_b) ? result : result.reverse();
 }
-
-function compare(num_a, num_b) {
-  return (num_a - num_b >= 0);
-}
-
-function judgeEven(num){
-  return num%2 == 0;
-}
-
 module.exports = get_integer_interval_2;
