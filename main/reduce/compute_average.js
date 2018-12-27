12e4dd545dda032e3240e9bb1c06322f7b186ce2
XY-ZHOU <731986251@qq.com>
2018-12-27 16:37:14 +0800

    optimizate compute average

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -1,10 +1,9 @@
 'use strict';
 
 function compute_average(collection) {
-  let sum = 0;
-  for (let i = 0; i < collection.length; i++) {
-    sum += collection[i];
-  }
+  let sum = collection.reduce(function(acc, curValue, currentIndex, array) {
+    return acc + curValue;
+  });
   return (sum / collection.length);
 }
 module.exports = compute_average;
