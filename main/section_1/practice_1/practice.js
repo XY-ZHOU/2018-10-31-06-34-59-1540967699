f0c35727dbfa76da4b6ca611b79a6e13ecd7223f
XY-ZHOU <731986251@qq.com>
2018-12-27 16:45:40 +0800

    optimizate practice one in section one

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,15 +1,6 @@
 function collect_same_elements(collection_a, collection_b) {
-  let res=[];
-  for(let i = 0; i<collection_a.length;i++){
-    if(judgeStrInArr(collection_b,collection_a[i])){
-      res.push(collection_a[i]);
-    }
-  }
-  return res;
+  return collection_a.filter(function(element) {
+    return collection_b.includes(element);
+  });
 }
-
-function judgeStrInArr(arr, str) {
-  return (arr.indexOf(str) != -1);
-}
-
 module.exports = collect_same_elements;
