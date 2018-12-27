d8cab05ae881a96f4b3ea6269acd0f402cd09430
XY-ZHOU <731986251@qq.com>
2018-12-27 15:32:47 +0800

    modify get union

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,23 +1,9 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  let res=[];
-  outputElement(collection_a,res);
-  outputElement(collection_b,res);
-  return res;
+  let arr = collection_b.filter(function(element) {
+    return !collection_a.includes(element);
+  });
+  return collection_a.concat(arr);
 }
-
-function outputElement(arr,res){
-  for(let i = 0; i<arr.length; i++) {
-    if(judgeStrInArr(res, arr[i])){
-      res.push(arr[i]);
-    }
-  }
-}
-
-function judgeStrInArr(array, str) {
-  return (array.indexOf(str) == -1);
-}
-
 module.exports = get_union;
-
