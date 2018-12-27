fff62d7e3f79d577216a72a39f1ceebb14566204
XY-ZHOU <731986251@qq.com>
2018-12-27 16:38:04 +0800

    optimizate compute median

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -1,9 +1,13 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  collection.sort(sortAsc);
+  let lowMiddle = Math.floor((collection.length - 1) / 2);
+  let highMiddle = Math.ceil((collection.length - 1) / 2);
+  return (Number(collection[lowMiddle]) + Number(collection[highMiddle])) / 2;
 }
 
+function sortAsc(a, b) {
+  return a - b;
+}
 module.exports = compute_median;
-
-
