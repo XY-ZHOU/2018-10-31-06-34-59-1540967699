ffdb28c5b78a0248b2afcfff25e0e4d67b61a25b
XY-ZHOU <731986251@qq.com>
2018-12-27 16:37:45 +0800

    optimizate compute chain median

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -1,7 +1,13 @@
 'use strict';
 
-function compute_chain_median(collection) {
-  //在这里写入代码
+function compute_chain_median(chain) {
+  let arr = chain.split('->').sort(sortAsc);
+  let lowMiddle = Math.floor((arr.length - 1) / 2);
+  let highMiddle = Math.ceil((arr.length - 1) / 2);
+  return (Number(arr[lowMiddle]) + Number(arr[highMiddle])) / 2;
 }
 
+function sortAsc(a, b) {
+  return a - b;
+}
 module.exports = compute_chain_median;
