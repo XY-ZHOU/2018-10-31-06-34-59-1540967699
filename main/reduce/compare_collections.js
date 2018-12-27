08ee0499e53eb8272471ec33d082153b00b8caeb
XY-ZHOU <731986251@qq.com>
2018-12-27 16:36:39 +0800

    optimizate compare collections

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,19 +1,10 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  if (isSameLength(collection_a, collection_b)) {
+  if (collection_a.toString() != collection_b.toString()) {
     return false;
   } else {
-    for (let i = 0; i < collection_a.length; i++) {
-      if (collection_a[i] != collection_b[i]) {
-        return false;
-      }
-    }
     return true;
   }
-}
-
-function isSameLength(arr_a, arr_b) {
-  return arr_a.length != arr_b.length;
 }
 module.exports = compare_collections;
