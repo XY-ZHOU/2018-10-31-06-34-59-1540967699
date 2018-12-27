b40ac1354897f067a28e225e057cd161f8b6717b
XY-ZHOU <731986251@qq.com>
2018-12-27 15:40:14 +0800

    optimizate two collections practice one

---
+++ main/filter/two_collections_practice_1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function choose_common_elements(collection_a, collection_b) {
+  return collection_a.filter(function(element) {
+    return collection_b.includes(element);
+  });
+}
+module.exports = choose_common_elements;
