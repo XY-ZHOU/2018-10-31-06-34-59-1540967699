fc3e5d63b178727f3a42d2ac65cd6aeb0cf6c40f
XY-ZHOU <731986251@qq.com>
2018-12-27 18:22:02 +0800

    optimizate practice two in section two

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,37 +1,28 @@
 function count_same_elements(collection) {
-  let res = [];
-  let groupingCountObj = grouping_count(collection);
-  for (key in groupingCountObj) {
-    let obj = {};
-    obj.key = key;
-    obj.count = groupingCountObj[key];
-    res.push(obj);
-  }
-  return res;
+  return covertObjToArray(grouping_count(collection));
 }
 
 function grouping_count(collection) {
-  let groupingCountObj = {};
-  for (let i = 0; i < collection.length; i++) {
-    if (judgeStr(collection[i])) {
-      groupingCountObj[collection[i].slice(0, 1)] = Number(collection[i].slice(2));
+  let countObj = {};
+  collection.map((element) => {
+    let key = element.match(/[a-zA-Z]+/g)[0];
+    let matchValue = element.match(/[0-9]+/g);
+    let count = matchValue ? matchValue : 1;
+    if (countObj[key]) {
+      countObj[key] += parseInt(count);
     } else {
-      if (judgeKeyInObj(groupingCountObj, collection[i])) {
-        groupingCountObj[collection[i]] = 1;
-      } else {
-        groupingCountObj[collection[i]]++;
-      }
+      countObj[key] = parseInt(count);
     }
-  }
-  return groupingCountObj;
+  })
+  return countObj;
 }
 
-function judgeKeyInObj(obj, key) {
-  return (!obj.hasOwnProperty(key));
-}
-
-function judgeStr(str) {
-  var reg = /^[a-z]-\d{1,2}$/;
-  return reg.test(str);
+function covertObjToArray(obj) {
+  return Object.keys(obj).map((element) => {
+    return {
+      key: element,
+      count: obj[element]
+    };
+  });
 }
 module.exports = count_same_elements;
