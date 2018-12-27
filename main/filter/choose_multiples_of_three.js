c2200b924acad257fb6b58a9512a535e8b163afc
XY-ZHOU <731986251@qq.com>
2018-12-27 15:36:22 +0800

    optimizate choose nultiples of three

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,17 +1,8 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-  let arr = [];
-  for (let i = 0; i < collection.length; i++) {
-    if (judgeEven(parseInt(collection[i]))) {
-      arr.push(collection[i]);
-      }
-  }
-  return arr;
+  return collection.filter(function(element) {
+    return element % 3 === 0;
+  });
 }
-
-function judgeEven(num) {
-  return (num % 3 === 0);
-}
-
 module.exports = choose_multiples_of_three;
