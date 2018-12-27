b27fa4c68bd7fb9dc79703a564996412bd3664f8
XY-ZHOU <731986251@qq.com>
2018-12-27 15:31:54 +0800

    modify get letter interval two

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,28 +1,23 @@
 'use strict';
+const LETTER = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function get_letter_interval_2(number_a, number_b) {
-  if (number_a <= number_b) {
-    return outputString(number_a, number_b);
+  let start = (number_a >= number_b) ? number_b : number_a;
+  let end = (number_a >= number_b) ? number_a : number_b;
+  let result = [];
+  for (; start <= end; start++) {
+    result.push(getLetterByCode(start));
+  }
+  return (number_a <= number_b) ? result : result.reverse();
+}
+
+function getLetterByCode(code) {
+  if (code <= LETTER.length) {
+    return LETTER[code - 1];
   } else {
-    return outputString(number_b, number_a).reverse();
+    let firstLetter = LETTER[Math.floor((code - 1) / LETTER.length) - 1];
+    let secondLetter = LETTER[(code - 1) % LETTER.length];
+    return firstLetter + secondLetter;
   }
-}
-
-function outputString(num_a, num_b) {
-  let arr = [];
-  for (let i = num_a; i <= num_b; i++) {
-    if (i <= 26) {
-      arr.push(String.fromCharCode(i + 96));
-    } else if (26 < i && i <= 52) {
-      arr.push(numberToString(i, 1));
-    } else if (52 < i && i <= 78) {
-      arr.push(numberToString(i, 2));
-    }
-  }
-  return arr;
-}
-
-function numberToString(number, n) {
-  return String.fromCharCode(n + 96) + String.fromCharCode(number - 26 * n + 96);
 }
 module.exports = get_letter_interval_2;
