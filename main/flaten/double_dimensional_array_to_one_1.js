9061d2dcbeef946760a113bc337d43c9256696a7
XY-ZHOU <731986251@qq.com>
2018-12-27 15:53:59 +0800

    optimizate the first file of double dimensional array to one

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,25 +1,8 @@
 'use strict';
 
 function double_to_one(collection) {
-  let res=[];
-  for (let i = 0; i < collection.length; i++) {
-    if(isArrayOrNot(collection[i])){
-      outputElements(collection[i],res);
-    }else{
-      res.push(collection[i]);
-    }
-  }
-  return res;
+  return collection.reduce(function(acc, cur) {
+    return acc.concat(cur);
+  }, []);
 }
-
-function outputElements(arr,res){
-  for (let i = 0; i < arr.length; i++) {
-    res.push(arr[i]);
-  }
-}
-
-function isArrayOrNot(arr){
-  return (Object.prototype.toString.call(arr) == "[object Array]");
-}
-
 module.exports = double_to_one;
