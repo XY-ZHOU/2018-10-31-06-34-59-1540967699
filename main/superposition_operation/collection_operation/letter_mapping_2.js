a464303a734f53d3e2d22f7bd49b08f25e18665a
XY-ZHOU <731986251@qq.com>
2018-12-27 18:34:26 +0800

    modify letter mapping two

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,20 +1,13 @@
 'use strict';
 
 function average_to_letter(collection) {
-  let sum = 0;
-  let average = 0;
-  for (let i = 0; i < collection.length; i++) {
-    sum += collection[i];
-  }
-  if (judgeRemainder(sum, collection.length)) {
-    average = sum / collection.length;
-  } else {
-    average = sum / collection.length + 1;
-  }
-  return String.fromCharCode(average + 96);
+  return String.fromCharCode(compute_average(collection) + 96);
 }
 
-function judgeRemainder(num_a, num_b) {
-  return num_a % num_b == 0;
+function compute_average(collection) {
+  let sum = collection.reduce(function(acc, curValue, currentIndex, array) {
+    return acc + curValue;
+  });
+  return (sum / collection.length);
 }
 module.exports = average_to_letter;
