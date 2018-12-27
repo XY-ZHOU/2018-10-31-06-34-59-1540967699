8b3e18d3d43d0fd80eb886c62081af986a0f843e
XY-ZHOU <731986251@qq.com>
2018-12-27 15:28:55 +0800

    modify get interxection

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,17 +1,8 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  let arr = [];
-  for (let i = 0; i < collection_b.length; i++) {
-    if (judgeStrInArr(collection_a, collection_b[i])) {
-      arr.push(collection_b[i])
-    }
-  }
-  return arr;
+  return collection_b.filter(function(element) {
+    return collection_a.indexOf(element) != -1;
+  });
 }
-
-function judgeStrInArr(arr, str) {
-  return (arr.indexOf(str) != -1);
-}
-
 module.exports = get_intersection;
