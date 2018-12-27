a2a4f92b9e61feab822ec64fcedaa142ceaa9a99
XY-ZHOU <731986251@qq.com>
2018-12-27 18:35:11 +0800

    modify practice one

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,10 +1,9 @@
 'use strict';
 
 function hybrid_operation(collection) {
-  let sum = 0;
-  for (let i = 0; i < collection.length; i++) {
-    sum += collection[i];
-  }
+  let sum = collection.reduce(function(acc, curValue, currentIndex, array) {
+    return acc + curValue;
+  });
   return 3 * sum + 2 * collection.length;
 }
 module.exports = hybrid_operation;
