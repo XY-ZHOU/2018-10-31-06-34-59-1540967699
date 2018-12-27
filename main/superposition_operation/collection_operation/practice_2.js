139a74c8cb630870b7f0a6fb413f682f4a2a8c09
XY-ZHOU <731986251@qq.com>
2018-12-27 18:35:25 +0800

    modify practice two

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,17 +1,16 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-  let res = [];
-  for (let i = 0; i < collection.length; i++) {
-    if (judgeUneven(collection[i])) {
-      collection[i] = 3 * collection[i] + 2;
-      res.push(collection[i]);
-    }
+  let res = collect_all_odd(collection);
+  for (let element of collection) {
+    element = element * 3 + 2;
   }
   return res;
 }
 
-function judgeUneven(num) {
-  return num % 2 != 0;
+function collect_all_odd(collection) {
+  return collection.filter(function(element) {
+    return element % 2 != 0;
+  });
 }
 module.exports = hybrid_operation_to_uneven;
