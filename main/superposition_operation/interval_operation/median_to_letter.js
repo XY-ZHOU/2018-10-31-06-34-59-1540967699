7adde4fe2764cf30f82d9ea34cb3e158e86194fc
XY-ZHOU <731986251@qq.com>
2018-12-27 18:38:05 +0800

    modify median to letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,11 +1,23 @@
 'use strict';
+const LETTER = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function median_to_letter(collection) {
+  collection.sort(sortAsc);
   let num = collection[0] + collection.length / 2;
-  if (num <= 26) {
-    return String.fromCharCode(num + 96);
+  return getLetterByCode(num);
+}
+
+function getLetterByCode(code) {
+  if (code <= LETTER.length) {
+    return LETTER[code - 1];
   } else {
-    return "a" + String.fromCharCode(num + 70);
+    let firstLetter = LETTER[Math.floor((code - 1) / LETTER.length) - 1];
+    let secondLetter = LETTER[(code - 1) % LETTER.length];
+    return firstLetter + secondLetter;
   }
+}
+
+function sortAsc(a, b) {
+  return a - b;
 }
 module.exports = median_to_letter;
