f0b5cafcc4e342c1fab397fc6947cc23876f1df5
XY-ZHOU <731986251@qq.com>
2018-12-27 16:28:52 +0800

    optimizate map to three multiples

--- main/map/map_to_three_multiples.js
+++ main/map/map_to_three_multiples.js
@@ -1,8 +1,7 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return collections.map(function(item){
-    return item*3;
+var map_to_three_multiples = function(collections) {
+  return collections.map(function(item) {
+    return item * 3;
   });
 };
-
 module.exports = map_to_three_multiples;
